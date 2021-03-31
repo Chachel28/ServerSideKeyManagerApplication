18bfc0fc1f13665927bfd42730ef41ca88b79027
Chachel28 <alexgb11@gmail.com>
2021-03-31 13:38:03 +0200

    Initial commit

---
+++ src/main/kotlin/es/chachel/keymanager/ServerSideKeyManagerAppApplication.kt
@@ -0,0 +1,11 @@
+package es.chachel.keymanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerSideKeyManagerAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerSideKeyManagerAppApplication>(*args)
+}
---
+++ src/test/kotlin/es/chachel/keymanager/ServerSideKeyManagerAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package es.chachel.keymanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerSideKeyManagerAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
